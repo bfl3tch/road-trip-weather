7950a6b2bf0a9bd11b6b093017297a8c6cc27ed0
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-27 11:31:36 -0400

    Setup route for booksearch

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :forecast, only: [:index]
+      get 'book-search', to: 'booksearch#index'
     end
   end
 end
