cfddfa792e869c076f1abd7e64fa7d136c742b66
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-27 01:59:26 -0400

    Add conditional, rescue, and error message/generator

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::ForecastController < ApplicationController
   before_action :weather_determinator, only: [:index]
+rescue_from NoMethodError, with: :error_generator
 
   def index
-    render json: ForecastSerializer.new(@weather)
-    render json: ForecastSerializer.new(@error) if @error
+    render json: ForecastSerializer.new(@weather) if @weather
   end
 
   private
 
   def weather_determinator
-    ForecastFacade.lat_long(params[:location]) if (params[:location] && !params[:location].nil?)
-    @weather = ForecastFacade.weather_getter
+     (params[:location] && !params[:location].nil?)
+      ForecastFacade.lat_long(params[:location])
+      @weather = ForecastFacade.weather_getter
+  end
+
+  def error_generator
+    render json: {error: { error_message: 'Invalid Entry' } }
   end
 end
