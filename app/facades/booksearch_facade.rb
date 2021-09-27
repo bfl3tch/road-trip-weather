b6b534fdc369a19e45f462484f5a883920eb5a6a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-27 12:35:49 -0400

    Weather attribute added and formatted

--- app/facades/booksearch_facade.rb
+++ app/facades/booksearch_facade.rb
@@ -2,7 +2,12 @@ class BooksearchFacade
   def self.get_books(query, quantity)
     response = BookService.get_books(query, quantity)
     lat_long = LatLongService.get_lat_long(query)
-    forecast = ForecastService.get_forecast
+
+    lat = lat_long[:results].first[:locations].first[:latLng][:lat]
+    long = lat_long[:results].first[:locations].first[:latLng][:lng]
+
+    weather = ForecastService.get_forecast(lat, long)
+    forecast = weather[:current]
     BooksResult.new(response, quantity, forecast)
   end
 end
