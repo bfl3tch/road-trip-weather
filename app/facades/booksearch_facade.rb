2fccfe7a411669961bbe062e0f11dce3f14a73a0
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-27 13:36:12 -0400

    Testing

--- app/facades/booksearch_facade.rb
+++ app/facades/booksearch_facade.rb
@@ -2,12 +2,14 @@ class BooksearchFacade
   def self.get_books(query, quantity)
     response = BookService.get_books(query, quantity)
     lat_long = LatLongService.get_lat_long(query)
-
     lat = lat_long[:results].first[:locations].first[:latLng][:lat]
     long = lat_long[:results].first[:locations].first[:latLng][:lng]
-    destination = "#{query}"
+
     weather = ForecastService.get_forecast(lat, long)
     forecast = weather[:current]
+
+    destination = "#{query}"
+
     BooksResult.new(response, quantity, forecast, destination)
   end
 end
