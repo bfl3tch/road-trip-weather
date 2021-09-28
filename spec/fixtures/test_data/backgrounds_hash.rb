0749bd9d7b7dc0e2ce895fe4919a96c7c188cb3c
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-27 23:42:49 -0400

    Create fixture for backgrounds

---
+++ spec/fixtures/test_data/backgrounds_hash.rb
@@ -0,0 +1,93 @@
+{
+    "data": {
+        "id": null,
+        "type": "backgrounds",
+        "attributes": {
+            "image": [
+                {
+                    "id": "UdohIQA3MxA",
+                    "created_at": "2021-08-07T13:00:56-04:00",
+                    "updated_at": "2021-09-27T02:26:41-04:00",
+                    "promoted_at": null,
+                    "width": 5616,
+                    "height": 3744,
+                    "color": "#f3f3f3",
+                    "blur_hash": "LGE:h#pI_4nM9GITD$IT-=?It8M_",
+                    "description": "Coconut Palm Tree, Marco Island, FL. July 2021",
+                    "alt_description": "green palm tree during daytime",
+                    "urls": {
+                        "raw": "https://images.unsplash.com/photo-1628354973088-ded60ff905bb?ixid=MnwyNjMyMjd8MHwxfHNlYXJjaHwxfHxtYXJjbyUyMGlzbGFuZCUyQyUyMGZsfGVufDB8fHx8MTYzMjc5MDQ3Mg&ixlib=rb-1.2.1",
+                        "full": "https://images.unsplash.com/photo-1628354973088-ded60ff905bb?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjMyMjd8MHwxfHNlYXJjaHwxfHxtYXJjbyUyMGlzbGFuZCUyQyUyMGZsfGVufDB8fHx8MTYzMjc5MDQ3Mg&ixlib=rb-1.2.1&q=85",
+                        "regular": "https://images.unsplash.com/photo-1628354973088-ded60ff905bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjMyMjd8MHwxfHNlYXJjaHwxfHxtYXJjbyUyMGlzbGFuZCUyQyUyMGZsfGVufDB8fHx8MTYzMjc5MDQ3Mg&ixlib=rb-1.2.1&q=80&w=1080",
+                        "small": "https://images.unsplash.com/photo-1628354973088-ded60ff905bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjMyMjd8MHwxfHNlYXJjaHwxfHxtYXJjbyUyMGlzbGFuZCUyQyUyMGZsfGVufDB8fHx8MTYzMjc5MDQ3Mg&ixlib=rb-1.2.1&q=80&w=400",
+                        "thumb": "https://images.unsplash.com/photo-1628354973088-ded60ff905bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjMyMjd8MHwxfHNlYXJjaHwxfHxtYXJjbyUyMGlzbGFuZCUyQyUyMGZsfGVufDB8fHx8MTYzMjc5MDQ3Mg&ixlib=rb-1.2.1&q=80&w=200"
+                    },
+                    "links": {
+                        "self": "https://api.unsplash.com/photos/UdohIQA3MxA",
+                        "html": "https://unsplash.com/photos/UdohIQA3MxA",
+                        "download": "https://unsplash.com/photos/UdohIQA3MxA/download",
+                        "download_location": "https://api.unsplash.com/photos/UdohIQA3MxA/download?ixid=MnwyNjMyMjd8MHwxfHNlYXJjaHwxfHxtYXJjbyUyMGlzbGFuZCUyQyUyMGZsfGVufDB8fHx8MTYzMjc5MDQ3Mg"
+                    },
+                    "categories": [],
+                    "likes": 0,
+                    "liked_by_user": false,
+                    "current_user_collections": [],
+                    "sponsorship": null,
+                    "topic_submissions": {},
+                    "user": {
+                        "id": "0F20ZworKVE",
+                        "updated_at": "2021-09-22T07:06:47-04:00",
+                        "username": "christinsuzanne",
+                        "name": "Christin Wurst",
+                        "first_name": "Christin",
+                        "last_name": "Wurst",
+                        "twitter_username": null,
+                        "portfolio_url": "http://www.christinsuzanne.com",
+                        "bio": null,
+                        "location": "Nashville, TN",
+                        "links": {
+                            "self": "https://api.unsplash.com/users/christinsuzanne",
+                            "html": "https://unsplash.com/@christinsuzanne",
+                            "photos": "https://api.unsplash.com/users/christinsuzanne/photos",
+                            "likes": "https://api.unsplash.com/users/christinsuzanne/likes",
+                            "portfolio": "https://api.unsplash.com/users/christinsuzanne/portfolio",
+                            "following": "https://api.unsplash.com/users/christinsuzanne/following",
+                            "followers": "https://api.unsplash.com/users/christinsuzanne/followers"
+                        },
+                        "profile_image": {
+                            "small": "https://images.unsplash.com/profile-1628378955521-82a44ce40b4fimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=32&w=32",
+                            "medium": "https://images.unsplash.com/profile-1628378955521-82a44ce40b4fimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64",
+                            "large": "https://images.unsplash.com/profile-1628378955521-82a44ce40b4fimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128"
+                        },
+                        "instagram_username": "Christinsuzannephotography",
+                        "total_collections": 0,
+                        "total_likes": 0,
+                        "total_photos": 10,
+                        "accepted_tos": true,
+                        "for_hire": false,
+                        "social": {
+                            "instagram_username": "Christinsuzannephotography",
+                            "portfolio_url": "http://www.christinsuzanne.com",
+                            "twitter_username": null,
+                            "paypal_email": null
+                        }
+                    },
+                    "tags": [
+                        {
+                            "type": "search",
+                            "title": "marco island"
+                        },
+                        {
+                            "type": "search",
+                            "title": "fl"
+                        },
+                        {
+                            "type": "search",
+                            "title": "usa"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
