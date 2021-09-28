57a90a23e5ad6cd842fb5e86ac77c0fb1523c105
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-28 14:38:39 -0400

    Sessions request spec updated with happy and sad path expectations

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -14,17 +14,51 @@ RSpec.describe "Sessions API" do
       @user = User.find(body[:data][:id].to_i)
     end
 
-    it 'happy path: creates session and logs a user in' do
-      post '/api/v1/sessions', params: {
-        email: "whatever@example.com",
-        password: "password",
-        password_confirmation: "password"
-      }
+    context 'happy path' do
+      it 'creates session and logs a user in' do
+        post '/api/v1/sessions', params: {
+          email: "whatever@example.com",
+          password: "password",
+        }
 
-      body = JSON.parse(response.body, symbolize_names: true)
+        body = JSON.parse(response.body, symbolize_names: true)
 
-      expect(body[:data][:attributes][:email]).to eq("ligma1@example.com")
-      expect(body[:data][:attributes][:api_key]).to eq(@user.api_key)
+        expect(body[:data][:attributes][:email]).to eq("whatever@example.com")
+        expect(body[:data][:attributes][:api_key]).to eq(@user.api_key)
+      end
+    end
+
+    context 'sad path' do
+      it 'returns a generic error message about failed login with wrong password' do
+        post '/api/v1/sessions', params: {
+          email: "whatever@example.com",
+          password: "badpass",
+        }
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(body[:error]).to eq("Unable to log in. Please try again.")
+      end
+
+      it 'returns a generic error message about failed login with wrong email' do
+        post '/api/v1/sessions', params: {
+          email: "bad@example.com",
+          password: "password",
+        }
+
+        body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(body[:error]).to eq("Unable to log in. Please try again.")
+      end
+
+      it 'returns a generic error message about failed login with no email' do
+        post '/api/v1/sessions', params: {
+          password: "password"
+        }
+
+        body = JSON.parse(response.body, symbolize_names: true)
+        expect(body[:error]).to eq("Unable to log in. Please try again.")
+      end
     end
   end
 end
