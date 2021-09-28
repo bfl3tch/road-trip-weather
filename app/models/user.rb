33ce91cf198b2fd082eb99b08111dd5d034d5c2c
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-28 03:50:45 -0400

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :email, :password, :password_confirmation, :api_key
-  validates_uniqueness_of :email, :api_key 
+  # validates_presence_of :email, :password, :password_confirmation, :api_key
+  # validates_uniqueness_of :email, :api_key
 end
