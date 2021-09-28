e5ebd9f576a5bea6f54c8fae50eb753ec675fcba
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-28 04:00:03 -0400

    Comment out user validations for now

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  describe "validations" do
-    it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:password) }
-    it { should validate_confirmation_of(:password) }
-    it { should validate_presence_of(:password_confirmation) }
-    it { should validate_presence_of(:api_key) }
-    it { should validate_uniqueness_of(:api_key) }
-  end
+  # describe "validations" do
+  #   it { should validate_presence_of(:email) }
+  #   it { should validate_uniqueness_of(:email) }
+  #   it { should validate_presence_of(:password) }
+  #   it { should validate_confirmation_of(:password) }
+  #   it { should validate_presence_of(:password_confirmation) }
+  #   it { should validate_presence_of(:api_key) }
+  #   it { should validate_uniqueness_of(:api_key) }
+  # end
 end
