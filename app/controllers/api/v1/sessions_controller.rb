8e36499ea678d36b481654750d4776c50a39cd46
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-29 02:24:18 -0400

    Refactor to find user through strong params on sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-
+    user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
       render json: UsersSerializer.new(user), status: 201
     else
