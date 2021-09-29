868284d7bb356d3ff081949c387dab1a1dc62df5
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-29 03:20:25 -0400

    Create serializer to display poro attributes

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,35 @@
+class RoadTripSerializer
+  include JSONAPI::Serializer
+  attributes :start_city, :end_city, :travel_time, :weather_at_eta
+
+  attribute :weather_at_eta do |weather|
+    if !weather.weather_at_eta.nil? && weather.weather_at_eta[:temp].class == (Float || Integer)
+      {
+        temperature: weather.weather_at_eta[:temp],
+        conditions: weather.weather_at_eta[:weather].first[:description]
+      }
+    elsif !weather.weather_at_eta.nil? && weather.weather_at_eta[:temp].class == Hash
+      {
+        temperature: weather.weather_at_eta[:temp][:day],
+        conditions: weather.weather_at_eta[:weather].first[:description]
+      }
+    else
+      {}
+    end
+  end
+
+  attribute :travel_time do |travel|
+    if travel.travel_time.length > 0
+      time = JSON.parse(travel.travel_time)
+      if time.length == 3
+        "#{time.first} hours, #{time.second} minutes"
+      elsif time.length == 2
+        "#{time.first} minutes"
+      elsif time.length == 1
+        "#{time.first} seconds"
+      end
+    else
+      "impossible route"
+    end
+  end
+end
