c31942123577ac15fc695a8bf8cc648d280ee956
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-29 05:30:03 -0400

    Clean up users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,21 @@
 class Api::V1::UsersController < ApplicationController
-
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      user.update(api_key: SecureRandom.hex)
-      render json: UsersSerializer.new(user), status: :created
-    else
-      render json: { error: "#{user.errors.full_messages.first}." }, status: :unprocessable_entity
-    end
+    @user.save ? assign_key_and_respond : error_generator
   end
 
   private
+
+  def assign_key_and_respond
+    @user.update(api_key: SecureRandom.hex)
+    render json: UsersSerializer.new(@user), status: :created
+  end
+
+  def error_generator
+    render json: { error: "#{@user.errors.full_messages.first}." },
+                   status: :unprocessable_entity
+  end
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
